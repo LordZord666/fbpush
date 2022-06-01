6f331aceec947f556dcc50b8fbecbfa4675f75a6
Pratik Dahal <36587251+pfdah@users.noreply.github.com>
2022-06-01 14:12:10 +0545

    update

--- tkpush.py
+++ tkpush.py
@@ -5,10 +5,11 @@ from dbConnection import getData
 page_access_token = "EAAF219B1ahEBAEYJB0h82ZAcEbrJAkXrLrZCSNu6KYUgP4I7H6NZAe6YZAvoa4EtJm6zKfUqzPSz05ZCnZBwKd9rP0MnDQwNF37fhjOsw95r2BoIw3agQZA3MotAueTEYmMLhNlFXb74lYSryqWrgoAzPoZAqi0N218LKVpbpwl71A34gG5OcCgAsAGibImGo0ZACLRiJDAoMagZDZD"
 graph = facebook.GraphAPI(page_access_token)
 facebook_page_id = "113544291367455"
-graph.put_object(facebook_page_id, "feed", message='test message')
+#graph.put_object(facebook_page_id, "feed", message='test message')
 
 data= getData()
+
+
 for data in myresult:
-  print(x)
-  #graph.put_object(facebook_page_id, "feed", message='test message')
+  graph.put_object(facebook_page_id, "feed", message=data)
   #yha message ko satta data anusar tanera hal

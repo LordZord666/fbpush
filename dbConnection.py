2fa7c92c56b77a1faaba7b6f4bfc0db0dbc5002b
Jordan Maharjan <70703500+LordZord666@users.noreply.github.com>
2022-06-06 16:00:34 +0545

    Update dbConnection.py

--- dbConnection.py
+++ dbConnection.py
@@ -1,18 +1,38 @@
+from multiprocessing import connection
+from sqlite3 import Cursor
 import mysql.connector
-import time
-import requests
+import cv2
 
-#connection = pyodbc.connect(driver='{ODBC driver 17 for SQL Server}', host="")
+def convertToBinary (filename):
+  with open(filename,'rb') as file:
+    binarydata= file.read()
+  return binarydata
+
+def convertBinaryToFile(binarydata, filename):
+  with open(filename,'wb') as file:
+    file.write(binarydata)
 
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="",
-  database="test"
+  password="", 
+  database="task"
 )
 
 def getData():
   mycursor = mydb.cursor()
-  mycursor.execute("SELECT * FROM Tab")
+  mycursor.execute("SELECT * FROM table_product")
   myresult = mycursor.fetchall()
+  print (myresult)
+  
+  insertQuery = """ INSERT into table_product (id, title, description, price, image, Availabitity) value(%s,%s,%s,%s,%s,%s)"""
+  img=cv2.imread('pic.jpg')
+  gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+  print(gray_img)
+  value = ('3,ball,basketball,500,convertPic,1')
+  Cursor.execute(insertQuery,value)
+  connection.commit
+  
   return myresult
+
+
